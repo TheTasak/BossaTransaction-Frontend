dce921aca6717714176d0ed31fd86d2820efb28f
Przemysław Tasarz <przemek433@interia.eu>
2023-03-13 16:55:37 +0100

    earnings from share, tree chart init

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,22 +1,68 @@
 import {useEffect, useState} from "react";
 import Transaction from "../helpers/models";
 import {getResource} from "../../config";
-import {calculateCurrentShares} from "../helpers/calculate";
+import {calculateCurrentShares, WalletShare} from "../helpers/calculate";
+import './Dashboard.scss';
+
+import {ResponsiveTreeMap} from '@nivo/treemap';
 
 
 const Dashboard = () => {
     const [transactions, setTransactions] = useState<Transaction[] | undefined>(undefined);
+    const [shares, setShares] = useState<WalletShare[] | undefined>(undefined);
 
     useEffect(() => {
         getResource("/transactions", setTransactions);
     }, []);
 
-    if (transactions !== undefined) {
-        calculateCurrentShares(transactions);
-    }
+    useEffect(() => {
+        if (transactions !== undefined) {
+            setShares(calculateCurrentShares(transactions));
+            console.log(shares);
+        }
+    }, [transactions]);
 
     return (
-        <div>
+        <div className="dashboard">
+            {
+                shares !== undefined &&
+                <ResponsiveTreeMap
+                    data={shares}
+                    identity="name"
+                    value="totalPrice"
+                    valueFormat=".02s"
+                    margin={{ top: 10, right: 10, bottom: 10, left: 10 }}
+                    labelSkipSize={12}
+                    labelTextColor={{
+                        from: 'color',
+                        modifiers: [
+                            [
+                                'darker',
+                                1.2
+                            ]
+                        ]
+                    }}
+                    parentLabelPosition="left"
+                    parentLabelTextColor={{
+                        from: 'color',
+                        modifiers: [
+                            [
+                                'darker',
+                                2
+                            ]
+                        ]
+                    }}
+                    borderColor={{
+                        from: 'color',
+                        modifiers: [
+                            [
+                                'darker',
+                                0.1
+                            ]
+                        ]
+                    }}
+                />
+            }
 
         </div>
     )
--- src/components/helpers/calculate.tsx
+++ src/components/helpers/calculate.tsx
@@ -1,14 +1,16 @@
 import Transaction from "./models";
 
-class WalletShare {
+export class WalletShare {
+    name : string;
     transactions : Transaction[];
     shares : number;
     totalPrice : number;
+    earnings : number;
 
     private calculateShares(): number {
         let shareSum : number = 0;
         this.transactions.map(transaction => {
-            if(transaction.t_type === "BUY") {
+            if (transaction.t_type === "BUY") {
                 shareSum += transaction.amount;
             } else if (transaction.t_type === "SELL") {
                 shareSum -= transaction.amount;
@@ -20,7 +22,7 @@ class WalletShare {
     private calculateTotalSum(): number {
         let priceSum : number = 0;
         this.transactions.map(transaction => {
-            if(transaction.t_type === "BUY") {
+            if (transaction.t_type === "BUY") {
                 priceSum += transaction.price * transaction.amount;
             } else if (transaction.t_type === "SELL") {
                 priceSum -= transaction.price * transaction.amount;
@@ -29,25 +31,55 @@ class WalletShare {
         return priceSum;
     }
 
+    private calculateEarnings(): number {
+        let earningsSum : number = 0;
+        let transactionShares = [...this.transactions];
+
+        this.transactions.filter(transaction => transaction.t_type === "SELL").map(transaction => {
+            let currentSharesAbleToSell = transactionShares.filter(transaction => transaction.t_type === "BUY" && transaction.amount > 0);
+            let sharesToSell : number = transaction.amount;
+            let i : number = 0;
+            while (sharesToSell > 0) {
+                if (currentSharesAbleToSell[i] !== undefined) {
+                    if (currentSharesAbleToSell[i].amount >= sharesToSell) {
+                        earningsSum += (currentSharesAbleToSell[i].price * sharesToSell) - (transaction.price * sharesToSell);
+                        currentSharesAbleToSell[i].amount -= sharesToSell;
+                        sharesToSell = 0;
+                        break;
+                    } else {
+                        earningsSum += (currentSharesAbleToSell[i].price * currentSharesAbleToSell[i].amount) - (transaction.price * currentSharesAbleToSell[i].amount);
+                        sharesToSell -= currentSharesAbleToSell[i].amount;
+                        currentSharesAbleToSell[i].amount = 0;
+                    }
+                } else {
+                    break;
+                }
+                i++;
+            }
+        });
+
+        return earningsSum;
+    }
+
     getAveragePrice(): string {
         return (this.totalPrice / this.shares).toFixed(2);
     }
     constructor(transactions : Transaction[]) {
+        this.name = transactions[0].name;
         this.transactions = transactions;
         this.shares = this.calculateShares();
         this.totalPrice = this.calculateTotalSum();
+        this.earnings = this.calculateEarnings();
     }
 
 }
 
-export const calculateCurrentShares = (data: Transaction[]) => {
+export const calculateCurrentShares = (data: Transaction[]) : WalletShare[] => {
     let sortedTransactions = data.filter(transaction => transaction.t_type === "BUY" || transaction.t_type === "SELL").sort((a,b) => {
         const dateA : Date = new Date(a.date);
         const dateB : Date = new Date(b.date);
         return dateA.getTime() - dateB.getTime();
     });
-
-    console.log(data);
 
     let transactionShares = sortedTransactions.map(transaction => transaction.name).filter((value, index, array) => array.indexOf(value) === index);
     let shares : WalletShare[] = [];
@@ -55,6 +87,7 @@ export const calculateCurrentShares = (data: Transaction[]) => {
     transactionShares?.map(shareName => {
         let share : WalletShare = new WalletShare(sortedTransactions?.filter(transaction => transaction.name === shareName));
         shares.push(share);
-        console.log(shareName + " " + share.shares)
     });
+
+    return shares;
 }
